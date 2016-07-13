bf5e06415524aee7ca65b5f0bf368aa55604a6c7
jsanc <joseluis.sanchez@ventura24.es>
2016-07-13 10:04:51 +0200

    Fixed wrong values in qierues for notification Signed-off-by: jsanc <joseluis.sanchez@ventura24.es>

--- NotificationService/src/main/java/com/codetron/cloud/queue/notification/NotificationRepository.java
+++ NotificationService/src/main/java/com/codetron/cloud/queue/notification/NotificationRepository.java
@@ -12,8 +12,6 @@ import java.util.List;
 @Repository
 public interface NotificationRepository extends JpaRepository<Notification,Long>{
 
-    List<Notification> findAllByUserIdId(final Long userId);
-
-    Notification findByDrawId(final Long drawId);
+    List<Notification> findByEmail(final String email);
 
 }
