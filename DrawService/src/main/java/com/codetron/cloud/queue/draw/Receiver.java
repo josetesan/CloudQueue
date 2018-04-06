6106db56e36623e1dd3964cf4ca9eac9239ec5ca
josete <muallin@gmail.com>
2018-04-06 21:04:55 +0200

    Upgrade to spring boot 1.5.11.RELEASE

--- DrawService/src/main/java/com/codetron/cloud/queue/draw/Receiver.java
+++ DrawService/src/main/java/com/codetron/cloud/queue/draw/Receiver.java
@@ -1,7 +1,7 @@
 /******************************************************************************
- * Copyright (C) 2005 - 2016 Ventura24 S.L.U.                                *
+ * Copyright (C) 2005 - 2016 ACME S.L.U.                                *
  * *
- * Copyright and license details are included in Ventura24 license file.     *
+ * Copyright and license details are included in ACME license file.     *
  ******************************************************************************/
 package com.codetron.cloud.queue.draw;
 
@@ -18,9 +18,9 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  *****************************************************************************
- *  Copyright (C) 2005 - 2016 Ventura24 S.L.U.                                *
+ *  Copyright (C) 2005 - 2016 ACME S.L.U.                                *
  *                                                                            *
- *  Copyright and license details are included in Ventura24 license file.     *
+ *  Copyright and license details are included in ACME license file.     *
  ******************************************************************************
  * Created by jsanc on 11/07/16.
  */
